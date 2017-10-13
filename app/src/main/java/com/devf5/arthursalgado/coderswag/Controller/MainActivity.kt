fe0d982b85c94b711324273e41ad95578e9cb73e
arthursalgado <arthursalgado@live.com>
2017-10-13 12:26:17 -0300

    adicionado layout inicial / adapter explicações

--- app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.devf5.arthursalgado.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.devf5.arthursalgado.coderswag.Model.Category
 import com.devf5.arthursalgado.coderswag.R
+import com.devf5.arthursalgado.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category> // Adapter é o que controla as informações do model pra ser mostrado na ListView (view)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ArrayAdapter(context, que tipo de view que será mostrado na ListView, qual dado que será mostrado)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
