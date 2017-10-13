d38e0df648db5b0bde60a262ee40caf2c8b93fbc
arthursalgado <arthursalgado@live.com>
2017-10-13 19:05:13 -0300

    antes de adicionar o controller do recycle view

--- app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,7 @@ package com.devf5.arthursalgado.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import com.devf5.arthursalgado.coderswag.Adapters.CategoryAdapter
-import com.devf5.arthursalgado.coderswag.Model.Category
 import com.devf5.arthursalgado.coderswag.R
 import com.devf5.arthursalgado.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,5 +20,14 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.categories)
                 //ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
         categoryListView.adapter = adapter
+
+
+        // Saber em qual clicou. Mas vamos utilizar outro método com recycle views
+//        categoryListView.setOnItemClickListener { parent, view, position, id ->
+//            val category = DataService.categories[position]
+//            Toast.makeText(this, "You clicked on ${category.title} !", Toast.LENGTH_SHORT).show()
+//        }
+
+
     }
 }
