276a0c4f2b118aa51fae9251abbb9cd8db5a2ac7
arthursalgado <arthursalgado@live.com>
2017-10-13 13:06:16 -0300

    adicionado a reutilização das células

--- app/src/main/java/com/devf5/arthursalgado/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/devf5/arthursalgado/coderswag/Adapters/CategoryAdapter.kt
@@ -20,16 +20,25 @@ class CategoryAdapter constructor(context: Context, categories: List<Category>) 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView : View
+        val holder: ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+        if(convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder // valor de cada view é o holder (unique value)
+        } else {
+            // Reutilizar a celular se já foi criada
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
 
         val category = categories[position]
-        categoryName.text = category.title
-
+        holder.categoryName?.text = category.title
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
 
         return  categoryView
     }
@@ -46,5 +55,8 @@ class CategoryAdapter constructor(context: Context, categories: List<Category>) 
         return categories.count()
     }
 
-
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+    }
 }
