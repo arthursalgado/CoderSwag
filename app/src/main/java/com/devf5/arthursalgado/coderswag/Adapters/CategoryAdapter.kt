29ac8fb05be947e77770a5a551c2ddefe9e3f658
arthursalgado <arthursalgado@live.com>
2017-10-13 19:51:13 -0300

    mudamos para o recycle view adapter

--- app/src/main/java/com/devf5/arthursalgado/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/devf5/arthursalgado/coderswag/Adapters/CategoryAdapter.kt
@@ -13,10 +13,7 @@ import com.devf5.arthursalgado.coderswag.R
 /**
  * Created by arthursalgado on 13/10/17.
  */
-class CategoryAdapter constructor(context: Context, categories: List<Category>) : BaseAdapter() {
-
-    val context = context
-    val categories = categories
+class CategoryAdapter constructor(val context: Context, val categories: List<Category>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView : View
--- app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/devf5/arthursalgado/coderswag/Controller/MainActivity.kt
@@ -2,14 +2,17 @@ package com.devf5.arthursalgado.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.devf5.arthursalgado.coderswag.Adapters.CategoryAdapter
+import com.devf5.arthursalgado.coderswag.Adapters.CategoryRecycleAdapter
 import com.devf5.arthursalgado.coderswag.R
 import com.devf5.arthursalgado.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter // Criamos um adapter específico para o layout do projeto
+    lateinit var adapter : CategoryRecycleAdapter // Criamos com o recycle
+            //CategoryAdapter // Criamos um adapter específico para o layout do projeto
             //ArrayAdapter<Category> // Adapter é o que controla as informações do model pra ser mostrado na ListView (view)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +20,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // ArrayAdapter(context, que tipo de view que será mostrado na ListView, qual dado que será mostrado)
-        adapter = CategoryAdapter(this, DataService.categories)
-                //ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
+        // Não é a melhor maneira de implementar
+        //ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
 
         // Saber em qual clicou. Mas vamos utilizar outro método com recycle views
 //        categoryListView.setOnItemClickListener { parent, view, position, id ->
